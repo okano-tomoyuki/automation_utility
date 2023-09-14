bb70cb5d8389daab66f19e34648360f6a846a108
okanotomoyuki <tomoyuki.okano@tsuneishi.com>
2023-09-14 10:04:40 +0900

    sampleファイルを追加した。

---
+++ include/share/company.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file company.hpp
+ * @author okano tomoyuki (okano.development@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _SHARE_COMPANY_HPP_
+#define _SHARE_COMPANY_HPP_
+
+#include <string>
+#include <vector>
+
+/** 共通変数用名前空間 */
+namespace Share
+{
+
+/** 社員構造体 */
+struct Person
+{
+    int id;                                     /**!< 8桁の社員ID */
+    std::string name;                           /**!< フルネーム "苗字 名前" */
+    std::string email;                          /**!< メールアドレス "aaa.bbb@ccc.jp" */
+    double tall;                                /**!< 身長[cm] */
+    double weight;                              /**!< 体重[kg] */
+};
+
+/** グループ構造体 */
+struct Group
+{
+    int id;                                     /**!< 4桁のグループID */
+    std::string name;                           /**!< グループ名 */
+    Person leader;                              /**!< グループ長 */
+    std::vector<Person> member;                 /**!< 課員 */
+};
+
+/** 部署構造体 */
+struct Department
+{
+    int id;                                     /**!< 4桁の部署ID */
+    std::string name;                           /**!< 部署名 */
+    Person leader;                              /**!< 部署長 */
+    std::vector<Group> groups;                  /**!< 傘下のグループ */
+};
+
+/** 会社構造体 */
+struct Company
+{
+    int id;                                     /**!< 4桁の会社ID */
+    std::string name;                           /**!< 会社名 */
+    Person president;                           /**!< 社長 */
+    std::vector<Department> departments;        /**!< 傘下の部署 */
+};
+
+} // Share
+
+#endif
